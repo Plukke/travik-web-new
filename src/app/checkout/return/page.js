12f03c60f55ee47091c2e1fbcb31c504c8950e58
Giovanny Bernal <giovas94@gmail.com>
2024-04-16 10:19:17 -0600

    minor changes

---
+++ src/app/checkout/return/page.js
@@ -0,0 +1,9 @@
+export default async function CheckoutReturn() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 space-y-4">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+        Verificando pago....
+      </h1>
+    </main>
+  );
+}
