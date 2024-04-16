9aa31e1d33dce5ce05db77ecca16bd04ad0e542c
Giovanny Bernal <giovas94@gmail.com>
2024-04-16 12:26:08 -0600

    payment->status change

--- src/app/api/stripe/webhook/route.js
+++ src/app/api/stripe/webhook/route.js
@@ -65,7 +65,7 @@ export async function POST(request) {
         id: paymentIntent.metadata.reservationId,
         provider: JSON.stringify({ ...data }),
         payment: JSON.stringify({
-          payment: "PAID",
+          status: "PAID",
           intent: paymentIntent,
         }),
       });
