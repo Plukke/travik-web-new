37c052c8b9983644f72f0d04923d34c1db258663
Giovanny Bernal <giovas94@gmail.com>
2024-05-03 06:20:46 -0600

    minor changes

--- src/components/BookingSummary.js
+++ src/components/BookingSummary.js
@@ -32,7 +32,7 @@ export default function BookingSummary({ reservationDetail }) {
                 accommodation?.checkIn,
                 "days"
               )}{" "}
-              | {accommodation?.checkIn} al {accommodation?.checkOut}
+              | Del {accommodation?.checkIn} al {accommodation?.checkOut}
             </dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
