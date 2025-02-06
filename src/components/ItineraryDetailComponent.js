5319b44ce59303ee39814ed557f294e238b76354
Giovanny Bernal <giovas94@gmail.com>
2025-02-06 07:20:03 -0600

    minor changes

--- src/components/ItineraryDetailComponent.js
+++ src/components/ItineraryDetailComponent.js
@@ -16,6 +16,7 @@ import {
 import { HeartIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { StarIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
+import LogoComponent from "./LogoComponent";
 
 const product = {
   name: "Zip Tote Basket",
@@ -60,6 +61,18 @@ const product = {
       ],
     },
     // More sections...
+    {
+      name: "Highlights",
+      items: [
+        "Multiple strap configurations",
+        "Spacious interior with top zip",
+        "Leather handle and tabs",
+        "Interior dividers",
+        "Stainless strap loops",
+        "Double stitched construction",
+        "Water-resistant",
+      ],
+    },
   ],
 };
 const relatedProducts = [
@@ -354,13 +367,7 @@ export default function ItineraryDetailComponent() {
           <div className="border-t border-gray-200 py-20">
             <div className="grid grid-cols-1 md:grid-flow-col md:auto-rows-min md:grid-cols-12 md:gap-x-8 md:gap-y-16">
               {/* Image section */}
-              <div className="col-span-1 md:col-span-2 lg:col-start-1 lg:row-start-1">
-                <img
-                  alt=""
-                  src="https://tailwindui.com/img/logos/mark.svg?color=orange&shade=600"
-                  className="h-8 w-auto"
-                />
-              </div>
+              <LogoComponent />
 
               {/* Sitemap sections */}
               <div className="col-span-6 mt-10 grid grid-cols-2 gap-8 sm:grid-cols-3 md:col-span-8 md:col-start-3 md:row-start-1 md:mt-0 lg:col-span-6 lg:col-start-2">
@@ -417,36 +424,6 @@ export default function ItineraryDetailComponent() {
                     ))}
                   </ul>
                 </div>
-              </div>
-
-              {/* Newsletter section */}
-              <div className="mt-12 md:col-span-8 md:col-start-3 md:row-start-2 md:mt-0 lg:col-span-4 lg:col-start-9 lg:row-start-1">
-                <h3 className="text-sm font-medium text-gray-900">
-                  Sign up for our newsletter
-                </h3>
-                <p className="mt-6 text-sm text-gray-500">
-                  The latest deals and savings, sent to your inbox weekly.
-                </p>
-                <form className="mt-2 flex sm:max-w-md">
-                  <label htmlFor="email-address" className="sr-only">
-                    Email address
-                  </label>
-                  <input
-                    id="email-address"
-                    type="text"
-                    required
-                    autoComplete="email"
-                    className="w-full min-w-0 appearance-none rounded-md border border-gray-300 bg-white px-4 py-2 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-orange-500"
-                  />
-                  <div className="ml-4 flex-shrink-0">
-                    <button
-                      type="submit"
-                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-                    >
-                      Sign up
-                    </button>
-                  </div>
-                </form>
               </div>
             </div>
           </div>
